3bb7f60f8f81a8c5cdb7160c9840dca9caddae1c
Fichen <davidfichtenbaum@gmail.com>
2020-06-30 16:40:16 -0300

    Video 23 - Event game creation

---
+++ app/Events/RemainingTimeChanged.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Support\Facades\Log;
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+
+class RamainingTimeChanged implements ShouldBroadcast
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    //Laravel broadcast all public properties and only these
+    public $time;
+
+    /**
+     * Create a new event instance.
+     *
+     * @return void
+     */
+    public function __construct($time)
+    {
+        \Log::debug("time: {$this->time}");
+        $this->time = $time;
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return \Illuminate\Broadcasting\Channel|array
+     */
+    public function broadcastOn()
+    {
+        return new Channel('game');
+    }
+}
