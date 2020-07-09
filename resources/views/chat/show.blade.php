2a467ddc1801f412fca0a9079f5583a0f13e1ec1
Fichen <davidfichtenbaum@gmail.com>
2020-07-08 23:47:57 -0300

    Video 30: broadcast message to all users and seeing it on browser

--- resources/views/chat/show.blade.php
+++ resources/views/chat/show.blade.php
@@ -46,6 +46,8 @@
 @push('scripts')
 <script>
     const usersElement = document.getElementById('users');
+    const messagesElement = document.getElementById('messages');
+
 
     Echo.join('chat')
     .here((users) => {
@@ -67,6 +69,14 @@
     .leaving((user) => {
         let element = document.getElementById(user.id);
         element.parentNode.removeChild(element);
+    })
+    .listen('MessageSent', (e) => {
+        let element = document.createElement('li');
+        element.setAttribute('id', e.user.id);
+        element.innerText = e.user.name + ': ' + e.message;
+
+        messagesElement.appendChild(element);
+        
     });
 </script>
 <script>
